c338620dfae685faea925c667d8dfe7017a0fdbf
Justine Francis <jfvalmores@gmail.com>
2020-06-18 20:43:37 +0800

    minor update

--- server/app/core/Database.js
+++ server/app/core/Database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .connect(`${process.env.MONGODB_URI}${process.env.DB_NAME}`, { useNewUrlParser: true })
   .then(() => console.log('Database connection successful.'))
   .catch(e => console.error('Database connection error.', e.message));
 
--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const dotenv = require('dotenv').config();
+const database = require('./app/core/Database');
 
 const cors = require('cors');
+const db = require('./app/core/Database');
 const jsonParser = bodyParser.json();
 const urlEncoded = bodyParser.urlencoded({ extended: true });
 
-const PORT = 8080;
+const PORT = process.env.PORT;
 
 
 const app = express();
@@ -17,6 +19,8 @@ app.use(cors());
 app.use(helmet());
 app.use(jsonParser);
 app.use(urlEncoded);
+
+db.once('error', console.error.bind(console, 'MongoDB connection failed:'));
 
 
 app.get('/', (req, res) => {
