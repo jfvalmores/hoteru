734c6567439c7a7f4c3ed82b1f8c8d011ee51db2
Justine Francis <jfvalmores@gmail.com>
2020-07-01 20:07:01 +0800

    es6 updates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-import './server';
+import server from './server';
+
+/**
+  app.use('/api', [
+    // ROUTES
+  ]);
+ */
+
+server.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+server.get('/api/test', (req, res) => {
+  const tests = require('./tests');
+  res.send(tests.run());
+});
+
+server.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
--- src/server.js
+++ src/server.js
@@ -1,17 +1,15 @@
 // require('dotenv').config();
 import 'dotenv/config';
-
-const helmet = require('helmet');
-const express = require('express');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const database = require('./app/core/Database');
+import helmet from 'helmet';
+import express from 'express';
+import bodyParser from 'body-parser';
+import session from 'express-session';
+import database from './app/core/Database';
 
 const cors = require('cors');
 const jsonParser = bodyParser.json();
 const urlEncoded = bodyParser.urlencoded({ extended: true });
 
-const PORT = process.env.PORT;
 const APP_SECRET = process.env.APP_SECRET;
 const APP_SESS_AGE = process.env.APP_SESS_AGE;
 const APP_CLIENT_URL = process.env.APP_CLIENT_URL;
@@ -21,13 +19,13 @@ const corsOptions = {
   origin: [APP_CLIENT_URL]
 };
 
-const app = express();
+const server = express();
 
-app.use(helmet());
-app.use(cors(corsOptions));
-app.use(jsonParser);
-app.use(urlEncoded);
-app.use(session({
+server.use(helmet());
+server.use(cors(corsOptions));
+server.use(jsonParser);
+server.use(urlEncoded);
+server.use(session({
   resave: true,
   saveUninitialized: false,
   secret: APP_SECRET,
@@ -36,21 +34,4 @@ app.use(session({
   }
 }));
 
-/**
-  app.use('/api', [
-    // ROUTES
-  ]);
- */
-
-database.once('error', console.error.bind(console, 'MongoDB connection failed!'));
-
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
-
-app.get('/api/test', (req, res) => {
-  const tests = require('./tests');
-  res.send(tests.run());
-});
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default server;
